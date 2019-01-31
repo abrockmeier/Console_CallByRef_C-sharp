3f7e01e1e689bf60ab076367eae0cb56c575e6f8
abrockmeier <alexander.brockmeier@ambicotech.com>
2019-01-31 21:27:04 +0100

    v1.1
    some more "sandboxing" and even some results on dealing with ref & regular way of using object-method´s!

--- Console_CallByRef_C-sharp/Program.cs
+++ Console_CallByRef_C-sharp/Program.cs
@@ -40,10 +40,49 @@ namespace Console_CallByRef_C_sharp
 			Demo1 object1 = new Demo1();
 			Demo2 object2 = new Demo2();
 			object2.ChangeValue(object1);
-			Console.WriteLine(object1.Value);
-			Console.WriteLine("Assigning an object to a parameter means that the " +
+
+			Console.WriteLine("object2.ChangeValue( object1) call:");
+
+			Console.Write(object1.Value);
+			Console.WriteLine(" = object1.Value");
+			Console.WriteLine(object1);
+			
+			Console.WriteLine("");
+			Console.WriteLine("TEST no .value for object");
+			Console.WriteLine("");
+
+			//Console.WriteLine("object2");
+			Console.WriteLine(object2);
+			Console.WriteLine("");
+
+			Console.WriteLine("object2.ChangeValue(ref object1) call:");
+			object2.ChangeValue(ref object1);
+			Console.Write(object1.Value);
+			Console.WriteLine(" = object1.Value");
+			
+			Console.WriteLine("");
+			Console.WriteLine	("Assigning an object to a parameter means that the " +
 								"reference to the object\nis passed as an argument, " +
 								"not just any value.");
+			Console.WriteLine("");
+
+			Console.WriteLine("TEST no~2a new obj3&4 as above, call of object4.ChangeValue(object3)");
+			Demo1 object3 = new Demo1();
+			Demo2 object4 = new Demo2();
+			object4.ChangeValue(object3);
+			Console.WriteLine(object3.Value);
+			Console.WriteLine("= object3.Value");
+			Console.WriteLine("");
+
+			Console.WriteLine("TEST no~2b same objects agian call of object4.ChangeValue(ref object3) agian!");
+			object4.ChangeValue(ref object3);
+			Console.WriteLine(object1.Value);
+
+			Console.WriteLine("= object.1.Value");
+
+			// When objects like (object1 & object3) are created from Class Demo1 they "inherit"? the Value = 500.
+			// The objects (object2 & object4)are created from Class Demo2 they will get assigned the Value of the Demo2
+			// class [by calling the ChangeValue Method with (ref Demo1 @object) they receive the object Value of 4711...]
 
 			Console.ReadLine();
 		}
@@ -57,8 +96,17 @@ namespace Console_CallByRef_C_sharp
 			public void ChangeValue(Demo1 @object)
 			{
 				// (commenting out the following line will result 4711...)
-				@object = new Demo1(); // Output will be 500 todo: understand why..! 
+				//@object = new Demo1(); // Output will be 500 todo: understand why..!
+				// both together commented or uncommented have the same result (500 & 4711)!
+				//@object.Value = 4711; // Q: if uncommented this has no (visible) changes for the Output! Why?
+			}
+
+			public void ChangeValue(ref Demo1 @object)
+			{
+				// (commenting out the following line will result 4711...)
+				//@object = new Demo1(); // commented out has no (visible) changes// todo: understand why..! 
 				@object.Value = 4711;
+
 			}
 		}
 
